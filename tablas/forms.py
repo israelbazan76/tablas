64a8d50c364063812cf1eadb6d20f377bc321b09
israelbazan76 <israelbazan76@gmail.com>
2021-09-12 00:34:56 -0300

    tablas v1

--- tablas/forms.py
+++ tablas/forms.py
@@ -1,25 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField,BooleanField,IntegerField
 from wtforms.fields.simple import TextAreaField
-from wtforms.validators import DataRequired, Email, Length, NumberRange,InputRequired
+from wtforms.validators import DataRequired, Length, NumberRange,InputRequired
 from wtforms.widgets.core import TextArea
 
 
-class SignupForm(FlaskForm):
-    name = StringField('Nombre', validators=[DataRequired(), Length(max=64)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Registrar')
-class PostForm(FlaskForm):
-    title = StringField('Titulo', validators=[DataRequired(),Length(max=128)])
-    title_slug = StringField('Titulo slug', validators=[Length(max=128)])
-    content = TextAreaField('Contenido')
-    submit = SubmitField('Guardar')
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(),Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Recuérdame')
-    submit = SubmitField('Login')
+
 class TablasForm(FlaskForm):
     number = IntegerField('Tabla del', validators=[InputRequired( message='*Requerido'),NumberRange(min=1,message='Ingrese un número igual o mayor a %(min)d')])
     submit = SubmitField('Ir')
